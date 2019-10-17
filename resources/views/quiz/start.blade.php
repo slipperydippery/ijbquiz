fc16278ed379aacc9e7d5de2526d56f18c75a6a1
Maarten de Jager <maartendejager@gmail.com>
2019-10-17 11:22:51 +0200

    adds welcome to quickscan

--- resources/views/quiz/start.blade.php
+++ resources/views/quiz/start.blade.php
@@ -4,7 +4,8 @@
 	<div class="container-fluid d-flex flex-column h-100">
 		<div class="row">
 	        <div class="col-8 offset-md-2 text-center w-100">
-				<h1 class="display-5 py-5"> Beantwoord 7 vragen en je hebt gratis een eerste indruk van wat jouw bedrijf al doet en waar je aan kunt denken om slimmer, efficiënter en duurzamer om te gaan met woon/werkverkeer, zakelijke ritten en/of transport. </h1>
+				<h1 class="py-5 display-3">Welcom bij de quickscan</h1>
+				<h2 class="display-5 pb-5"> Beantwoord 7 vragen en je hebt gratis een eerste indruk van wat jouw bedrijf al doet en waar je aan kunt denken om slimmer, efficiënter en duurzamer om te gaan met woon/werkverkeer, zakelijke ritten en/of transport. </h2>
 
 
 
