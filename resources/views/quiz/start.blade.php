c4c2d76591940185a5db4dbe90258e9177cd1057
Maarten de Jager <maartendejager@gmail.com>
2019-10-17 11:24:27 +0200

    adds welcome to quickscan

--- resources/views/quiz/start.blade.php
+++ resources/views/quiz/start.blade.php
@@ -7,8 +7,6 @@
 				<h1 class="py-5 display-3">Welcom bij de quickscan</h1>
 				<h2 class="display-5 pb-5"> Beantwoord 7 vragen en je hebt gratis een eerste indruk van wat jouw bedrijf al doet en waar je aan kunt denken om slimmer, efficiënter en duurzamer om te gaan met woon/werkverkeer, zakelijke ritten en/of transport. </h2>
 
-
-
 				<a href=" {{ route('location.show') }} " class="btn btn-primary btn-lg btn-block"> Doe de quickscan </a>
 	        </div>
 	    </div>
